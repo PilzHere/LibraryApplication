62a075ebc5862c41b8703aff5a5dc44446612617
Annika <a.staalhandske@gmail.com>
2021-01-27 11:19:36 +0100

    created Book objectclass

--- src/library/books/Book.java
+++ src/library/books/Book.java
@@ -1,2 +1,55 @@
 public class Book {
+    private String title;
+    private String author;
+    private String genres;
+    private boolean available;
+
+    public Book(String title, String author, String genres, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.genres = genres;
+        this.available = available;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genres='" + genres + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }

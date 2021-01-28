6929116efe89a377834e902917a6b46109e44113
PilzHere <contact@christianpilz.com>
2021-01-28 14:05:00 +0100

    Formated code and uncommented boolean isAdmin. Can delete boolean later.

--- src/library/users/Librarian.java
+++ src/library/users/Librarian.java
@@ -1,8 +1,8 @@
 package library.users;
 
 public class Librarian extends User {
-    public Librarian(String name) {
-        super(name, true);
+    public Librarian (String name) {
+        super(name/*, true*/);
     }
     // a
 }

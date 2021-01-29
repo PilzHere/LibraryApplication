3da2c545025aa00cd1595d968a676d80fab12027
aahJohnson <johnson9438@gmail.com>
2021-01-29 11:56:53 +0100

    Added search function for authors

--- src/LibraryApplication.java
+++ src/LibraryApplication.java
@@ -2,10 +2,5 @@ import library.Library;
 import library.Login;
 
 public class LibraryApplication {
-    public static void main(String[] args) {
-        Library lib = new Library();
-        lib.addStartBooks();
-        lib.searchBookTitle();
-        //Login login = new Login();
-    }
+    public static void main(String[] args) { Login login = new Login(); }
 }

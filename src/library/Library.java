89e98f77255c13291bdc632ae0bb8a9a15528544
Sandra Wahlgren <sandrawahlgren@gmail.com>
2021-02-02 08:53:22 +0100

    Fixed conflilcts and Feature FileUtils

--- src/library/Library.java
+++ src/library/Library.java
@@ -34,39 +34,28 @@ public class Library {
 
     HashMap<String, Book> bookCollection = new HashMap<>();
 
-    public void displayBookCollection() {
-        System.out.println("The Library have the following books: ");
 
-        List<Map.Entry<String, Book>> bookList =
-                bookCollection.entrySet().stream()
-                        .collect(Collectors.toList());
-        bookList.forEach(book -> System.out.println(book.getValue()));
-        //change property available to a better printout, ex. available: yes/no
-    }
-
-    //admin to remove book from bookCollection
-    public void removeBook() {
     //ADMIN METHODS
 
     //Admin to remove book from bookCollection
     public void removeBook () {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter title of the book you wish to remove: ");
-        String adminInput = scan.nextLine();
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter title of the book you wish to remove: ");
+            String adminInput = scan.nextLine();
 
-        if (validateStringInput(adminInput)) {
+            if (validateStringInput(adminInput)) {
 
-            Map.Entry<String, Book> foundBook = bookCollection.entrySet().stream()
-                    .filter(book -> book.getValue().getTitle().equalsIgnoreCase(adminInput))
-                    .findAny().orElse(null);
+                Map.Entry<String, Book> foundBook = bookCollection.entrySet().stream()
+                        .filter(book -> book.getValue().getTitle().equalsIgnoreCase(adminInput))
+                        .findAny().orElse(null);
 
-            if (foundBook != null) {
-                bookCollection.remove(foundBook);
-                System.out.println(adminInput + " was deleted from book collection.");
-            } else {
-                System.out.println("No book with title " + adminInput + " was found.");
-            }
+                if (foundBook != null) {
+                    bookCollection.remove(foundBook);
+                    System.out.println(adminInput + " was deleted from book collection.");
+                } else {
+                    System.out.println("No book with title " + adminInput + " was found.");
+                }
 
                 if ((bookCollection.containsKey(adminInput))) { //NOT working with lower case letters!!
 
@@ -82,12 +71,11 @@ public class Library {
                     //TODO Back to meny here
                 }
                 //TODO back to meny
-        }else{
-            System.out.println("No valid input");
-            //TODO Back to meny here
-        }
+            } else {
+                System.out.println("No valid input");
+                //TODO Back to meny here
+            }
     }
-
 
         //Admin - Add book
         public boolean addBook () {
@@ -277,55 +265,46 @@ public class Library {
             public void displayBookCollection () {
                 System.out.println("The Library have the following books: \n");
 
-        for (Map.Entry<String, Book> entry : bookCollection.entrySet()) {
-            if (entry.getValue().isAvailable() == true) {
-                System.out.println("Title: " + entry.getValue().getTitle() + " | Author: " + entry.getValue().getAuthor() +  " | Genres: " + entry.getValue().getGenres()+"\n");
-                System.out.printf("Title: %s Author: %s Genres: %s\n" + entry.getValue().getTitle() + entry.getValue().getAuthor() + entry.getValue().getGenres());
-
-                //TODO seperate method
                 List<Map.Entry<String, Book>> bookList =
                         bookCollection.entrySet().stream()
                                 .collect(Collectors.toList());
-
                 bookList.forEach(book -> System.out.println(book.getValue()));
                 //change property available to a better printout, ex. available: yes/no
-            }
-        }
-    }
+                }
+
 
     //method to set a collection of 20-30 books.
     public HashMap <String, Book> addStartBooks() {
 
-        bookCollection.put("Sofies World", new Book("Sofies World", "Jostein Gaarder", "Philosophy", true));
+        bookCollection.put("Sofies World", new Book("Sofies World", "Jostein Gaarder", "Philosophy", true, ""));
         bookCollection.put("Eileen",
-                new Book("Eileen", "Ottessa Moshfegh", "Fiction", true));
+                new Book("Eileen", "Ottessa Moshfegh", "Fiction", true, ""));
         bookCollection.put("Lord of the Rings: The Fellowship of the Rings",
-                new Book("Lord of the Rings: The Fellowship of the Rings", "J.R.R Toliken", "Fantasy, Classic", true));
+                new Book("Lord of the Rings: The Fellowship of the Rings", "J.R.R Toliken", "Fantasy, Classic", true, ""));
         bookCollection.put("Lord of the Rings: The Two Towers",
-                new Book("Lord of the Rings: The Two Towers", "J.R.R Toliken", "Fantasy, Classic", true));
+                new Book("Lord of the Rings: The Two Towers", "J.R.R Toliken", "Fantasy, Classic", true, ""));
         bookCollection.put("Lord of the Rings: The Return of the King",
-                new Book("Lord of the Rings: The Return of the King", "J.R.R Toliken", "Fantasy, Classic", true));
-        bookCollection.put("Alice in Wonderland", new Book("Alice in Wonderland", "Lewis Carroll", "Classic", true));
-        bookCollection.put("Crime and Punishment", new Book("Crime and Punishment", "Fjodor Dostojevskij", "Classic", true));
-        bookCollection.put("Coraline", new Book("Coraline", "Neil Gaiman", "Fantasy", true));
-        bookCollection.put("Siddhartha", new Book("Siddhartha", "Hermann Hesse", "Philosophy, Classic", true));
-        bookCollection.put("Malmcolm X", new Book("Malmcolm X", "Manning Marable", "Biography", true));
-        bookCollection.put("The Age of Bowie", new Book("The Age of Bowie", "Paul Morley", "Biography", true));
-        bookCollection.put("Martin Luther King: a self-biograhpy", new Book("Martin Luther King: a self-biograhpy", "Martin Luther King", "Biography", true));
-        bookCollection.put("No Logo", new Book("No Logo", "Naomi Klein", "Non-fiction", true));
-        bookCollection.put("This Changes Everything", new Book("This Changes Everything", "Naomi Klein", "Non-fiction", true));
-        bookCollection.put("I am Malala", new Book("I am Malala", "Malala Yousafzai", "Non-fiction, Biography", true));
-        bookCollection.put("Carrie", new Book("Carrie", "Stephen King", "Horror", true));
-        bookCollection.put("It", new Book("It", "Stephen King", "Horror", true));
-        bookCollection.put("The Shining", new Book("The Shining", "Stephen King", "Horror", true));
-        bookCollection.put("The Bell Jar", new Book("The Bell Jar", "Sylvia Plath", "Modern Classic, Fiction", true));
-        bookCollection.put("The Sellout", new Book("The Sellout", "Paul Beatty", "Fiction", true));
-        bookCollection.put("The Luminaries", new Book("The Luminaries", "Elenor Catton", "Fiction", true));
-        bookCollection.put("The Plague", new Book("The Plague", "Albert Camus", "Modern Classic", true));
-        bookCollection.put("Nocturner", new Book("Nocturner", "Kazuo Ishiguro", "Modern Classic", true));
+                new Book("Lord of the Rings: The Return of the King", "J.R.R Toliken", "Fantasy, Classic", true, ""));
+        bookCollection.put("Alice in Wonderland", new Book("Alice in Wonderland", "Lewis Carroll", "Classic", true, ""));
+        bookCollection.put("Crime and Punishment", new Book("Crime and Punishment", "Fjodor Dostojevskij", "Classic", true, ""));
+        bookCollection.put("Coraline", new Book("Coraline", "Neil Gaiman", "Fantasy", true, ""));
+        bookCollection.put("Siddhartha", new Book("Siddhartha", "Hermann Hesse", "Philosophy, Classic", true, ""));
+        bookCollection.put("Malmcolm X", new Book("Malmcolm X", "Manning Marable", "Biography", true, ""));
+        bookCollection.put("The Age of Bowie", new Book("The Age of Bowie", "Paul Morley", "Biography", true, ""));
+        bookCollection.put("Martin Luther King: a self-biograhpy", new Book("Martin Luther King: a self-biograhpy", "Martin Luther King", "Biography", true, ""));
+        bookCollection.put("No Logo", new Book("No Logo", "Naomi Klein", "Non-fiction", true, ""));
+        bookCollection.put("This Changes Everything", new Book("This Changes Everything", "Naomi Klein", "Non-fiction", true, ""));
+        bookCollection.put("I am Malala", new Book("I am Malala", "Malala Yousafzai", "Non-fiction, Biography", true, ""));
+        bookCollection.put("Carrie", new Book("Carrie", "Stephen King", "Horror", true, ""));
+        bookCollection.put("It", new Book("It", "Stephen King", "Horror", true, ""));
+        bookCollection.put("The Shining", new Book("The Shining", "Stephen King", "Horror", true, ""));
+        bookCollection.put("The Bell Jar", new Book("The Bell Jar", "Sylvia Plath", "Modern Classic, Fiction", true, ""));
+        bookCollection.put("The Sellout", new Book("The Sellout", "Paul Beatty", "Fiction", true, ""));
+        bookCollection.put("The Luminaries", new Book("The Luminaries", "Elenor Catton", "Fiction", true, ""));
+        bookCollection.put("The Plague", new Book("The Plague", "Albert Camus", "Modern Classic", true, ""));
+        bookCollection.put("Nocturner", new Book("Nocturner", "Kazuo Ishiguro", "Modern Classic", true, ""));
 
         return bookCollection;
     }
-
 
 }

d87942b8d3c964c00c90effd28058187cd8150f8
aahJohnson <johnson9438@gmail.com>
2021-02-08 13:38:55 +0100

    Cleanup round 2

--- src/library/Library.java
+++ src/library/Library.java
@@ -394,22 +394,12 @@ public class Library {
                 .stream().collect(Collectors.toList());
         listByTitle.sort(Comparator.comparing(book -> (book.getValue().getTitle())));
 
-        String checkAvailable;
-
         for (int i = 0; i < listByTitle.size(); i++) {
-            if (listByTitle.get(i).getValue().isAvailable() == true) {
-                checkAvailable = "Available!";
-            }
-            else {
-                checkAvailable = "Not available";
-            }
             System.out.println(
                     "Title: " + listByTitle.get(i).getValue().getTitle() +
                             "\nAuthor: " + listByTitle.get(i).getValue().getAuthor() +
-                            "\nGenre: " + listByTitle.get(i).getValue().getGenres() +
                             "\n");
         }
-        //change property available to a better printout, ex. available: yes/no
     }
 
     // List all books alphabetically sorted by author
@@ -418,19 +408,10 @@ public class Library {
                 .stream().collect(Collectors.toList());
         listByAuthor.sort(Comparator.comparing(book -> (book.getValue().getAuthor())));
 
-        String checkAvailable;
-
         for (int i = 0; i < listByAuthor.size(); i++) {
-            if (listByAuthor.get(i).getValue().isAvailable() == true) {
-                checkAvailable = "Available!";
-            }
-            else {
-                checkAvailable = "Not available";
-            }
             System.out.println(
                     "Author: " + listByAuthor.get(i).getValue().getAuthor() +
                             "\nTitle: " + listByAuthor.get(i).getValue().getTitle() +
-                            "\nGenre: " + listByAuthor.get(i).getValue().getGenres() +
                     "\n");
         }
     }
